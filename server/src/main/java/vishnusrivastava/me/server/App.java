3fc2f75dd76bc9783f54c562c69826e1bb13c7d2
zxingz <vishnu-srivastava@hotmail.com>
2017-04-04 19:09:18 -0400

    server first commit !

--- server/src/main/java/vishnusrivastava/me/server/App.java
+++ server/src/main/java/vishnusrivastava/me/server/App.java
@@ -4,10 +4,30 @@ package vishnusrivastava.me.server;
  * Hello world!
  *
  */
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Helslo World!" );
-    }
+	public static void main(String[] args) throws Exception {
+	    HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+	    server.createContext("/", new MyHandler());
+	    server.setExecutor(null); // creates a default executor
+	    server.start();
+	  }
+
+	  static class MyHandler implements HttpHandler {
+	    public void handle(HttpExchange t) throws IOException {
+	      byte [] response = "<H1>Hi, this is my page !</H1>".getBytes();
+	      t.sendResponseHeaders(200, response.length);
+	      OutputStream os = t.getResponseBody();
+	      os.write(response);
+	      os.close();
+	    }
+	  }
 }
