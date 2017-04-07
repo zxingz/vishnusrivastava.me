721b5d56db0cff9b7ba133c3060078452551495a
Administrator <Administrator@172.31.30.155>
2017-04-07 17:50:51 +0000

    fb hook

--- server/src/main/java/vishnusrivastava/me/server/App.java
+++ server/src/main/java/vishnusrivastava/me/server/App.java
@@ -1,10 +1,12 @@
 package vishnusrivastava.me.server;
 
+import java.io.ByteArrayOutputStream;
 /**
  * Hello world!
  *
  */
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
@@ -16,14 +18,17 @@ public class App
 {
 	public static void main(String[] args) throws Exception {
 	    HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
-	    server.createContext("/", new MyHandler());
+	    server.createContext("/fbhook", new MyHandler());
 	    server.setExecutor(null); // creates a default executor
 	    server.start();
 	  }
 
 	  static class MyHandler implements HttpHandler {
-	    public void handle(HttpExchange t) throws IOException {
-	      byte [] response = "<H1>Hi, this is my page !</H1>".getBytes();
+	    @SuppressWarnings("restriction")
+		public void handle(HttpExchange t) throws IOException {
+	      System.out.println(t.getRequestURI().getQuery());
+	      String query[] = t.getRequestURI().getQuery().split("&");
+	      byte [] response = query[1].split("=")[1].getBytes();
 	      t.sendResponseHeaders(200, response.length);
 	      OutputStream os = t.getResponseBody();
 	      os.write(response);
